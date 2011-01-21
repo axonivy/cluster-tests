d5682e0445a5be3dd42de0b2be162b2a5fd6f70a
Flavio Sadeghi <flavio.sadeghi@axonivy.com>
2011-01-21 11:14:03 +0000

    * Story 272 - Testen des Cluster Servers - added testcases to test Intermediate and Start event bean reviewed by rwei

---
+++ ServerTest/src/ch/ivyteam/ivy/cluster/eventbean/AssertEventBeanLog.java
@@ -0,0 +1,128 @@
+package ch.ivyteam.ivy.cluster.eventbean;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Asserts the created log the startup and shutdown behavior of 
+ * Start- and Intermediate-Event-Beans with two clusters.
+ * <p>
+ * This class is called from the ClusterTests.xml as a JUnit Test.
+ * In this ant-script also the startup and shutdown of the nodes 
+ * is implemented.
+ * </p> 
+ * @author fs
+ * @since 19.01.2011
+ */
+public class AssertEventBeanLog extends Assert
+{
+  
+  /**
+   * This class implements the assertion of the log file with the
+   * generated remote log file.
+   * @throws Exception
+   */
+  @Test
+  public void testClusterIntermediateEventBean() throws Exception
+  {
+    assertBean("ClusterIntermediateEventBean");
+  }
+  
+  /**
+   * This class implements the assertion of the log file with the
+   * generated remote log file.
+   * @throws Exception
+   */
+  @Test
+  public void testClusterStartEventBean() throws Exception
+  {
+    assertBean("ClusterStartEventBean");
+  }
+  
+  /**
+   * This class implements the assertion of the log file with the
+   * generated remote log file.
+   * @throws Exception
+   */
+  @Test
+  public void testMasterIntermediateEventBean() throws Exception
+  {
+    assertBean("MasterIntermediateEventBean");
+  }
+
+  /**
+   * This class implements the assertion of the log file with the
+   * generated remote log file.
+   * @throws Exception
+   */
+  @Test
+  public void testMasterStartEventBean() throws Exception
+  {
+    assertBean("MasterStartEventBean");
+  }
+
+  /**
+   * @param beanName 
+   * @throws IOException 
+   * @throws URISyntaxException 
+   * @throws Exception 
+   */
+  private void assertBean(String beanName) throws Exception
+  {
+    // Check node 1
+    assertLog(beanName, 1, System.getProperty("node1.baseUri", "http://localhost:18080"));
+    // Check node 2
+    assertLog(beanName, 2, System.getProperty("node2.baseUri", "http://localhost:18081"));
+  }
+  
+  /**
+   * Asserts the content log of the remote node with the 'local log file'.
+   * @param beanName 
+   * @param nodeId 
+   * @param remoteBaseUri 
+   * @throws URISyntaxException 
+   * @throws IOException 
+   * @throws MalformedURLException 
+   */
+  private void assertLog(String beanName, int nodeId, String remoteBaseUri) throws URISyntaxException, IOException, MalformedURLException
+  {
+    String remoteUrl = remoteBaseUri + "/ivy/page/System/TestCasesPerformance/log/statechange_"+ beanName +".log";
+    String localFilePath = "Assert" + beanName +"-node"+ String.valueOf(nodeId) + ".log";
+    URI expectedFileUrl = AssertEventBeanLog.class.getResource(localFilePath).toURI();
+    String expected = FileUtils.readFileToString(new File(expectedFileUrl));
+    String actual = getRemoteContent(remoteUrl);
+
+    assertEquals("Start or stop of Start- or Entermediate-Event-Beans on cluster node " + nodeId, expected.replace("\r\n", "\n"), actual);
+  }
+
+  /**
+   * @param uri
+   * @return The log content of the given node at the remote cluster
+   * @throws MalformedURLException
+   * @throws IOException
+   */
+  private String getRemoteContent(String uri) throws MalformedURLException, IOException
+  {
+    InputStream is = null;
+    URL url = new URL(uri);
+    try
+    {
+      is = url.openStream();
+      return IOUtils.toString(is);
+    }
+    finally
+    {
+      IOUtils.closeQuietly(is);
+    }
+  }
+}
---
+++ ServerTest/src/ch/ivyteam/ivy/cluster/eventbean/readme-StartAndStopSequenze.txt
@@ -0,0 +1,55 @@
+
+------------------------------------------------------------------------------------------------
+
+* STOP N2 - 1
+NODE 2: Bean 'ClusterStartEventBean' on node 'zugpcfs.soreco.wan:2' is 'STOPPING'
+NODE 2: Bean 'ClusterIntermediateEventBean' on node 'zugpcfs.soreco.wan:2' is 'STOPPING'
+* START N2 - 2
+NODE 2: Bean 'ClusterStartEventBean' on node 'zugpcfs.soreco.wan:2' is 'STARTING'
+NODE 2: Bean 'ClusterIntermediateEventBean' on node 'zugpcfs.soreco.wan:2' is 'STARTING'
+
+* STOP N1 - 3
+NODE 1: Bean 'ClusterStartEventBean' on node 'zugpcfs.soreco.wan:1' is 'STOPPING'
+NODE 1: Bean 'MasterStartEventBean' on node 'zugpcfs.soreco.wan:1' is 'STOPPING'
+NODE 1: Bean 'ClusterIntermediateEventBean' on node 'zugpcfs.soreco.wan:1' is 'STOPPING'
+NODE 1: Bean 'MasterIntermediateEventBean' on node 'zugpcfs.soreco.wan:1' is 'STOPPING'
+NODE 2: Bean 'MasterStartEventBean' on node 'zugpcfs.soreco.wan:2' is 'STARTING'
+NODE 2: Bean 'MasterIntermediateEventBean' on node 'zugpcfs.soreco.wan:2' is 'STARTING'
+
+* START N1 - 4
+NODE 1: Bean 'ClusterStartEventBean' on node 'zugpcfs.soreco.wan:1' is 'STARTING'
+NODE 1: Bean 'ClusterIntermediateEventBean' on node 'zugpcfs.soreco.wan:1' is 'STARTING'
+
+* STOP N1 - 5
+NODE 1: Bean 'ClusterStartEventBean' on node 'zugpcfs.soreco.wan:1' is 'STOPPING'
+NODE 1: Bean 'ClusterIntermediateEventBean' on node 'zugpcfs.soreco.wan:1' is 'STOPPING'
+
+* START N1 - 6
+NODE 1: Bean 'ClusterStartEventBean' on node 'zugpcfs.soreco.wan:1' is 'STARTING'
+NODE 1: Bean 'ClusterIntermediateEventBean' on node 'zugpcfs.soreco.wan:1' is 'STARTING'
+
+* STOP N2 - 7
+NODE 2: Bean 'ClusterStartEventBean' on node 'zugpcfs.soreco.wan:2' is 'STOPPING'
+NODE 2: Bean 'MasterStartEventBean' on node 'zugpcfs.soreco.wan:2' is 'STOPPING'
+NODE 2: Bean 'ClusterIntermediateEventBean' on node 'zugpcfs.soreco.wan:2' is 'STOPPING'
+NODE 2: Bean 'MasterIntermediateEventBean' on node 'zugpcfs.soreco.wan:2' is 'STOPPING'
+NODE 1: Bean 'MasterStartEventBean' on node 'zugpcfs.soreco.wan:1' is 'STARTING'
+NODE 1: Bean 'MasterIntermediateEventBean' on node 'zugpcfs.soreco.wan:1' is 'STARTING'
+
+* START N2 - 2
+NODE 2: Bean 'ClusterStartEventBean' on node 'zugpcfs.soreco.wan:2' is 'STARTING'
+NODE 2: Bean 'ClusterIntermediateEventBean' on node 'zugpcfs.soreco.wan:2' is 'STARTING'
+
+------------------------------------------------------------------------------------------------
+NOTE: Logout put of START and STOP of a single server
+** STARTING:
+Bean 'ClusterStartEventBean' on node 'zugpcfs.soreco.wan:1' is 'STARTING'
+Bean 'MasterStartEventBean' on node 'zugpcfs.soreco.wan:1' is 'STARTING'
+Bean 'ClusterIntermediateEventBean' on node 'zugpcfs.soreco.wan:1' is 'STARTING'
+Bean 'MasterIntermediateEventBean' on node 'zugpcfs.soreco.wan:1' is 'STARTING'
+
+** STOPPING:
+Bean 'ClusterStartEventBean' on node 'zugpcfs.soreco.wan:1' is 'STOPPING'
+Bean 'MasterStartEventBean' on node 'zugpcfs.soreco.wan:1' is 'STOPPING'
+Bean 'ClusterIntermediateEventBean' on node 'zugpcfs.soreco.wan:1' is 'STOPPING'
+Bean 'MasterIntermediateEventBean' on node 'zugpcfs.soreco.wan:1' is 'STOPPING'
